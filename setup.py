a1e86d53311748b7ec8884c667972b4549907a51
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2022-09-02 15:57:19 +0930

    DEV: make pip friendly

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+packages = setuptools.find_packages()
+package_data = {"condiga_utils": ["condiga_utils/*"]}
+
+data_files = [(".", ["LICENSE", "README.md"])]
+
+setuptools.setup(
+    name="condiga",
+    version="0.1",
+    zip_safe=True,
+    author="Vijini Mallawaarachchi and Yu Lin",
+    author_email="viji.mallawaarachchi@gmail.com",
+    description="ConDiGA: Contigs directed gene annotation for accurate protein sequence database construction in metaproteomics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/metagentools/ConDiGA",
+    license="MIT",
+    packages=packages,
+    package_data=package_data,
+    data_files=data_files,
+    include_package_data=True,
+    scripts=["condiga"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "biopython",
+        "tqdm",
+        "XlsxWriter",
+        "click"
+    ],
+    python_requires=">=3.8",
+)

c91faac2047ce801f0b9399511747678308bf989
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2022-09-07 12:32:34 +0930

    DEV: detect k value from contig IDs

--- condiga_utils/contig_utils.py
+++ condiga_utils/contig_utils.py
@@ -1,11 +1,21 @@
-import subprocess
+import re
 
 from Bio import SeqIO
 
 def get_contig_lengths(contigs):
     contig_lengths = {}
 
+    record_id = ""
+
     for index, record in enumerate(SeqIO.parse(contigs, "fasta")):
         contig_lengths[record.id] = len(record.seq)
 
-    return contig_lengths
+        if record_id == "":
+            record_id = record.id
+
+    start_n = "k"
+    end_n = "_"
+    
+    k_val = int(re.search(r'%s(.*)%s' % (start_n, end_n), record_id).group(1))
+
+    return contig_lengths, k_val

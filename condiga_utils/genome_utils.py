8c905c1900ed9a20b7fe5cdb2f9e803b2ec18592
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2023-02-26 17:36:05 +1030

    REL: update version

--- condiga_utils/genome_utils.py
+++ condiga_utils/genome_utils.py
@@ -9,7 +9,7 @@ import sys
 from datetime import datetime
 
 # create logger
-logger = logging.getLogger("condiga 0.1.0")
+logger = logging.getLogger("condiga 0.2.0")
 
 
 def download_genomes(taxid_list, assembly_summary, output):
